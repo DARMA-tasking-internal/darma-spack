bb41ef99565174d954019cdb8f766b32fc29ac05
jjwilke <jjwilke@sandia.gov>
2018-09-04 17:06:13 -0700

    added fmt optional build for darma-futures

--- packages/fmt/package.py
+++ packages/fmt/package.py
@@ -25,48 +25,24 @@
 
 from spack import *
 
-class DarmaFutures(CMakePackage):
-    """A serialization library for migrating data between processes
-       in DARMA programs
+class Fmt(CMakePackage):
+    """An open-source package for type-safe
+       printf-style printing to screen/strings
     """
 
-    homepage = "https://github.com/DARMA-tasking"
-    url      = "https://github.com/DARMA-tasking/darma-futures"
+    homepage = "https://github.com/fmtlib/fmt"
+    url      = "https://github.com/fmtlib/fmt"
 
     version('1.0',
-        git='https://github.com/DARMA-tasking/darma-futures',
+        git='https://github.com/fmtlib/fmt', 
         branch='master')
 
-    variant("debugprint", values=int, default=0,
-             description="Specify whether to enable debug printing features")
-
-    depends_on('darma-serialization@1.0')
-    depends_on('fmt', when="+debugprint")
+    #depends_on()
 
     def cmake_args(self):
-      import sys
       import os
       spec = self.spec
-      debug = int(self.spec.variants['debugprint'].value)
-      if self.compiler.name == "clang":
-        if self.compiler.version < Version("3.6"):
-          raise Exception("DARMA requires Clang version >= 3.6")
-      elif self.compiler.name == "gcc":
-        if self.compiler.version < Version("5"):
-          raise Exception("DARMA requires GCC version >= 5")
-      else:
-        raise Exception("Incompatible compiler: need GCC >= 5, Clang >= 3.9")
-
-      utilityPath=os.path.join(spec['darma-serialization'].prefix, "cmake")
-
-      args = [
-        '-DDarmaSerialization_DIR=%s' % utilityPath,
-      #  '-DCMAKE_C_COMPILER=%s' % spec['mpi'].mpicc,
-      #  '-DCMAKE_CXX_COMPILER=%s' % spec['mpi'].mpicxx,
-      ]
-      if debug:
-        fmtPath=spec['fmt'].prefix
-        args.append("-Dfmt_DIR=%s" % fmtPath)
+      args = []
       return args
 
 
